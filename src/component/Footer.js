54e95846f9474ce3429fd4306e3c856e536c913e
khashayar <khashayarmobarezbusiness@gmail.com>
2024-01-16 21:49:16 +0330

    final

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className={styles.Container}>
             <h3 className={styles.FooterMader}>made by khashayar mobarez</h3>
-            <h3 className={styles.FooterMader}>khashayar sample product</h3>
+            <h3 className={styles.FooterMader}>sample product store</h3>
         </div>
     );
 };
